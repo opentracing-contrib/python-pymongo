bc2e6d60eea2bf5ec45d96e671176ba26817d5bc
Ryan Fitzpatrick <rmfitzpatrick@signalfx.com>
2019-01-25 14:10:58 -0500

    Bump version
    Adds sensitive command handling

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ integration_test_requirements = ['docker']
 
 setup(
     name='PyMongo-OpenTracing',
-    version='0.0.1',
+    version='0.0.2',
     url='http://github.com/signalfx/python-pymongo',
     download_url='http://github.com/signalfx/python-pymongo/tarball/master',
     author='SignalFx, Inc.',
